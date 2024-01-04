41cdaa2e04b870c59644bfc3ec7f33c8a25cb11c
Claire AVILA <avila.claire@gmail.com>
2024-01-04 14:02:15 +0100

    ajout du playAgain

--- src/jeuNim.java
+++ src/jeuNim.java
@@ -6,49 +6,55 @@ public static void main (String [] args) {
 	
 	//initialisation des deux joueurs 
 	Scanner scanner = new Scanner (System.in);
-	int initialAllumettes =21;
-
-	
-	int players = 1;//petite modif ici
-	
 	
 	System.out.println("joueur 1, indiquez votre nom");
 	String name1 = scanner.nextLine();
 	System.out.println("joueur 2, quel est votre nom ?");
 	String name2= scanner.nextLine();
 	System.out.println("Bienvenue " + name1 + " et "+  name2 + " au jeu du Nim !");
+	System.out.println("voulez-vous jouer ? oui/non");
+	String playAgain = scanner.nextLine();
 	
-	// explication des rêgles 
+	while(playAgain.equals("oui")) {
+		
 	
-	System.out.println("Dans ce jeu, nous avons un tas de 21 allumettes. Chaques joueur peut enlever entre 1 et 4 allumettes. Le perdant sera celui qui enlève la dernière.");
+		int initialAllumettes =21;
+		int players = 1;//petite modif ici
+	
+	
+		// explication des rêgles 
+	
+		System.out.println("Dans ce jeu, nous avons un tas de 21 allumettes. Chaques joueur peut enlever entre 1 et 4 allumettes. Le perdant sera celui qui enlève la dernière.");
 
-
+	
 	//création du jeu 
 
-	while(initialAllumettes > 1) {
-		System.out.println("Allumettes restantes" + initialAllumettes);
-		System.out.println("joueur " + players + " " + (players == 1 ? name1 : name2)  +" combien d'allumettes souhaitez vous retirer ?");// si joueur = 1 alors c'est joueur 1 sinon c'est joueur 2
+		while(initialAllumettes > 1) {
+			System.out.println("Allumettes restantes" + initialAllumettes);
+			System.out.println("joueur " + players + " " + (players == 1 ? name1 : name2)  +" combien d'allumettes souhaitez vous retirer ?");// si joueur = 1 alors c'est joueur 1 sinon c'est joueur 2
 	
-		int choise = scanner.nextInt();
+			int choise = scanner.nextInt();
 	
 		//on vérifie si le choix est bon
 	
-		if (choise >=1 && choise <= 4 && choise <= initialAllumettes) {
-			initialAllumettes -= choise; //	soustrait deux valeurs et stocke le résultat dans la variable
-			players = (players ==1) ? 2:1; //ça sert à changer de joueur et variable ternaire pour remplacer if else
+			if (choise >=1 && choise <= 4 && choise <= initialAllumettes) {
+				initialAllumettes -= choise; //	soustrait deux valeurs et stocke le résultat dans la variable
+				players = (players ==1) ? 2:1; //ça sert à changer de joueur et variable ternaire pour remplacer if else
 		
-		} else {
-			System.out.println("choix invalide");
-		
-		}
-			if (initialAllumettes <=1 || choise==initialAllumettes) {
-				System.out.println((players == 1 ? name1 :name2)+ " vous avez perdu");
-				}
+			} else {
+				System.out.println("choix invalide");
 		
 			}
-	
+			if (initialAllumettes <=1 || choise==initialAllumettes) {
+				System.out.println((players == 1 ? name1 :name2)+ " vous avez perdu");
+			}
+		
 		}
-				
+	System.out.println("voulez-vous rejouer");
+	playAgain = scanner.nextLine(); //ça marche pas :o
+	}
+	System.out.println("au revoir !");
+}	
 }
 
 
@@ -59,3 +65,4 @@ public static void main (String [] args) {
 //2celui qui enlève la dernière a perdu - ok
 // gérer le fait que si il reste 4 allumettes, on ne peut pas en prendre 4 ou dire si on a perdu - ok
 // gérer quand on a qu'une allumette on a perdu - ok
+//proposer au joueur de rejouer 
