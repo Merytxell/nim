dc101c953fcde5027de8f715934d1efba3042c5c
Claire AVILA <avila.claire@gmail.com>
2024-01-04 11:36:38 +0100

    ajout de l'annonce du perdant

--- src/jeuNim.java
+++ src/jeuNim.java
@@ -7,7 +7,7 @@ public static void main (String [] args) {
 	//initialisation des deux joueurs 
 	Scanner scanner = new Scanner (System.in);
 	int initialAllumettes =21;
-	int restAllumettes = 0;
+	//int restAllumettes = 0; sert à rien au final
 	
 	int players = 1;//petite modif ici
 	//System.out.println("le nombre d'allumettes est de " + initialAllumettes);
@@ -25,29 +25,38 @@ public static void main (String [] args) {
 
 //création du jeu 
 
-while(initialAllumettes >0) {
-	System.out.println("Allumettes restantes" + initialAllumettes);
-	System.out.println("joueur" + players +  (players == 1 ? name1 : name2)  +"combien d'allumettes souhaitez vous retirer ?");
+	while(initialAllumettes >0) {
+		System.out.println("Allumettes restantes" + initialAllumettes);
+		System.out.println("joueur" + players +  (players == 1 ? name1 : name2)  +"combien d'allumettes souhaitez vous retirer ?");// si joueur = 1 alors c'est joueur 1 sinon c'est joueur 2
 	
-	int choise = scanner.nextInt();
+		int choise = scanner.nextInt();
 	
-	//on vérifie si le choix est bon
+		//on vérifie si le choix est bon
 	
-	if (choise >=1 && choise <= 4 && choise <= initialAllumettes) {
-		initialAllumettes -= choise; //	soustrait deux valeurs et stocke le résultat dans la variable
-		players = (players ==1) ? 2:1; //ça sert à changer de joueur et variable ternaire pour remplacer if else
+		if (choise >=1 && choise <= 4 && choise <= initialAllumettes) {
+			initialAllumettes -= choise; //	soustrait deux valeurs et stocke le résultat dans la variable
+			players = (players ==1) ? 2:1; //ça sert à changer de joueur et variable ternaire pour remplacer if else
 		
-	}else {
-		System.out.println("choix invalide");
+		} else {
+			System.out.println("choix invalide");
 		
-
+		}
 	}
-}
-}
+	
+		// annoncer le gagnant 
+			System.out.println("allumettes restantes : " + initialAllumettes);
+			System.out.println("joueur" + players + (players == 1 ? name1 : name2) + "vous avez retiré la dernière allumette vous avez perdu");
+		}
+			
+		
 }
 
-//créer un programme avec 21 alumettes 
+
+
+
+
+//créer un programme avec 21 alumettes - ok
 //doit arbitrer une partie entre 2 humains dont le nom est demandé au démarrage - ok
 //exposer les règles  - ok
-//1 on peut retirer 1 à 4 allumette
+//1 on peut retirer 1 à 4 allumette - ok
 //2celui qui enlève la dernière a perdu
